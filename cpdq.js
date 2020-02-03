f0dff48ff905c0690e5417c205f90eda1c1d4a9d
photonmang <42340256+photonmang@users.noreply.github.com>
2020-02-03 22:20:28 +0800

    Update cpdq.js

--- cpdq.js
+++ cpdq.js
@@ -2,10 +2,10 @@
 菜谱大全 unlock vip
 
 #菜谱大全解锁vip
-https?:\/\/api\.jiaonizuocai\.com url script-response-body cpdq.js
+https?:\/\/api\.jiaonizuocai\.com url script-response-body https://raw.githubusercontent.com/photonmang/quantumultX/master/cpdq.js
 
 #Surge4.0
-^https?:\/\/api\.jiaonizuocai\.com requires-body=1,max-size=0,script-path=cpdq.js
+^https?:\/\/api\.jiaonizuocai\.com requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/photonmang/quantumultX/master/cpdq.js
 
 
 MITM = api.jiaonizuocai.com
