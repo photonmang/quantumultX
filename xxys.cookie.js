878eb071468b3f2af286f564203234ce09d7bbf3
photonmang <42340256+photonmang@users.noreply.github.com>
2020-06-24 17:15:04 +0800

    Create xxys.cookie.js
    获取小小影视cookie

---
+++ xxys.cookie.js
@@ -0,0 +1,57 @@
+const cookieName = '小小影视'
+const signurlKey = 'py_signurl_xxys'
+const signheaderKey = 'py_signheader_xxys'
+const py = init()
+
+if ($request && $request.method != 'OPTIONS') {
+  const signurlVal = $request.url
+  const signheaderVal = JSON.stringify($request.headers)
+  if (signurlVal) py.setdata(signurlVal, signurlKey)
+  if (signheaderVal) py.setdata(signheaderVal, signheaderKey)
+  py.msg(cookieName, `获取Cookie: 成功`, ``)
+}
+
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+}
+py.done()
