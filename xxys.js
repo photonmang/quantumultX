509e819e8781c8e6d993cb4c6bc8fca257ecfda7
photonmang <42340256+photonmang@users.noreply.github.com>
2020-10-15 17:31:10 +0800

    更新最新TF版去广告

--- xxys.js
+++ xxys.js
@@ -1,16 +1,17 @@
 /*
-XiaoXiaoYingShi unlock Vip
+更新：2020-10-15
+小小影视 unlock Vip&免广告
+感谢@GB-png，提供新方法
 QX:
-https:\/\/.*\..*\.com\/(vod\/reqplay\/|ucp/index|getGlobalData) url script-response-body https://raw.githubusercontent.com/photonmang/quantumultX/master/xxys.js
+https:\/\/.*\..*\.com\/(ssp-svr\/ssp\/list3|ucp/index) url script-response-body https://raw.githubusercontent.com/photonmang/quantumultX/master/xxys.js
 Surge:
-http-response https:\/\/.*\..*\.com\/(vod\/reqplay\/|ucp/index|getGlobalData) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/photonmang/quantumultX/master/xxys.js
+http-response https:\/\/.*\..*\.com\/(ssp-svr\/ssp\/list3|ucp/index) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/photonmang/quantumultX/master/xxys.js
 
-MITM = *.xxjjappss.com
+MITM = *.xxjjappss.com,*.huaerdadi.com
 */
 
 const path1 = "/ucp/index";
-const path2 = "/vod/reqplay/";
-const ad = 'getGlobalData';
+const ad = 'ssp-svr/ssp/list3';
 let obj = JSON.parse($response.body);
 
 if ($request.url.indexOf(path1) != -1){
@@ -20,16 +21,9 @@ if ($request.url.indexOf(path1) != -1){
 	obj.data.user["isvip"] = "1";
 	obj.data.user["goldcoin"] = "666";
 }
-if ($request.url.indexOf(path2) != -1){
-	obj.retcode = "0";
-	obj.data.lastplayindex = "1";
-	if(obj.data.hasOwnProperty("httpurl_preview")){
-		var playurl = obj.data["httpurl_preview"];
-		obj.data["httpurl"] = playurl;
-	};
-}
 
 if ($request.url.indexOf(ad) != -1) {
-delete obj.data.iOS_adgroups
+delete obj.data.pmap
 }
+
 $done({body: JSON.stringify(obj)});
