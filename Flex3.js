3b39071c5ec6f06cf8792b868246da8d1982fdf3
photonmang <42340256+photonmang@users.noreply.github.com>
2020-10-16 09:50:31 +0800

    Flex3解锁ban设备

--- Flex3.js
+++ Flex3.js
@@ -1,6 +1,9 @@
+/*
 #Flex3解锁ban设备 
 https://api2.getflex.co url script-request-body https://raw.githubusercontent.com/photonmang/quantumultX/master/Flex3.js
 
+MITM = api2.getflex.co
+*/
 const deviceID = "6c90e64fa0e65e75f743fb8a40f4edb88fe3c2df"; //如再次被Ban，请修改里面任意一个数字或字母
 let url = $request.url;
 let body = $request.body;
