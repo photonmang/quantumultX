382c1b797de3326258e32a80881336fd7728544b
photonmang <42340256+photonmang@users.noreply.github.com>
2020-12-03 20:19:23 +0800

    调整通知

--- cache/qqreads.js
+++ cache/qqreads.js
@@ -20,10 +20,8 @@ const jsname='企鹅读书'
 const $ = Env(jsname)
 
 const logs = 0;   //0为关闭日志，1为开启
-const notifyInterval=1
-//0为关闭通知，1为所有通知，2为宝箱领取成功通知，3为宝箱每18次通知一次
 
-
+const notifyInterval=$.getdata('notifyInterval'); 
 
 const dd=1//单次任务延迟,默认1秒
 
