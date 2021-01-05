565f3e08e0b02f03d2739fc25bb820b51dc72016
photonmang <42340256+photonmang@users.noreply.github.com>
2021-01-05 19:21:26 +0800

    Create qqreadCOOKIE.js

---
+++ cache/qqreadtest/qqreadCOOKIE.js
@@ -0,0 +1,16 @@
+//⚠️ck在``里面填写ck，多账号换行
+let qqreadbodyVal = ``;
+
+let qqreadtimeurlVal = ``;
+
+let qqreadtimeheaderVal = ``;
+
+let qqreadcookie = {
+        qqreadbodyVal: qqreadbodyVal,
+        qqreadtimeurlVal: qqreadtimeurlVal,
+        qqreadtimeheaderVal: qqreadtimeheaderVal
+}
+
+module.exports =  qqreadcookie
+  
+
