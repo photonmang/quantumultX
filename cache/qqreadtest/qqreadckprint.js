cdc38426d2f5d311b70067673b82bc89343f6be8
photonmang <42340256+photonmang@users.noreply.github.com>
2021-01-05 20:22:36 +0800

    Update qqreadckprint.js

--- cache/qqreadtest/qqreadckprint.js
+++ cache/qqreadtest/qqreadckprint.js
@@ -1,10 +1,4 @@
 /* 
-脚本改版至ziye
-
-
-
-
-
 
 使用说明:
 1.本脚本仅为打印QQ阅读Cookiek，无其他功能
@@ -38,73 +32,7 @@ let qqreadtimeheaderVal = "";
 let qqreadBD = [];
 let qqreadtimeURL = [];
 let qqreadtimeHD = [];
-if ($.isNode()) {
-  if (process.env.COOKIES_SPLIT) {
-    COOKIES_SPLIT = process.env.COOKIES_SPLIT;
-  }
-  console.log(
-    `============ cookies分隔符为：${JSON.stringify(
-      COOKIES_SPLIT
-    )} =============\n`
-  );
-  if (
-    process.env.QQREAD_BODY &&
-    process.env.QQREAD_BODY.indexOf(COOKIES_SPLIT) > -1
-  ) {
-    qqreadBD = process.env.QQREAD_BODY.split(COOKIES_SPLIT);
-  } else {
-    qqreadBD = process.env.QQREAD_BODY.split();
-  }
 
-  if (
-    process.env.QQREAD_TIMEURL &&
-    process.env.QQREAD_TIMEURL.indexOf(COOKIES_SPLIT) > -1
-  ) {
-    qqreadtimeURL = process.env.QQREAD_TIMEURL.split(COOKIES_SPLIT);
-  } else {
-    qqreadtimeURL = process.env.QQREAD_TIMEURL.split();
-  }
-
-  if (
-    process.env.QQREAD_TIMEHD &&
-    process.env.QQREAD_TIMEHD.indexOf(COOKIES_SPLIT) > -1
-  ) {
-    qqreadtimeHD = process.env.QQREAD_TIMEHD.split(COOKIES_SPLIT);
-  } else {
-    qqreadtimeHD = process.env.QQREAD_TIMEHD.split();
-  }
-}
-
-if ($.isNode()) {
-  Object.keys(qqreadBD).forEach((item) => {
-    if (qqreadBD[item]) {
-      qqreadbdArr.push(qqreadBD[item]);
-    }
-  });
-  Object.keys(qqreadtimeURL).forEach((item) => {
-    if (qqreadtimeURL[item]) {
-      qqreadtimeurlArr.push(qqreadtimeURL[item]);
-    }
-  });
-  Object.keys(qqreadtimeHD).forEach((item) => {
-    if (qqreadtimeHD[item]) {
-      qqreadtimehdArr.push(qqreadtimeHD[item]);
-    }
-  });
-
-  console.log(
-    `============ 共${qqreadtimehdArr.length}个企鹅读书账号  =============\n`
-  );
-  console.log(
-    `============ 脚本执行-北京时间(UTC+8)：${new Date(
-      new Date().getTime() + 8 * 60 * 60 * 1000
-    ).toLocaleString()}  =============\n`
-  );
-} else {
-  qqreadbdArr.push($.getdata("qqreadbd"));
-  qqreadtimeurlArr.push($.getdata("qqreadtimeurl"));
-  qqreadtimehdArr.push($.getdata("qqreadtimehd"));
-  // 根据boxjs中设置的额外账号数，添加存在的账号数据进行任务处理
   let zhs = ($.getval('zhs') || '1') - 0;
   for (let i = 1; i <= zhs; i++) {
     if ($.getdata(`qqreadbd${i}`)) {
@@ -113,8 +41,6 @@ if ($.isNode()) {
       qqreadtimehdArr.push($.getdata(`qqreadtimehd${i}`));
     }
   }
-}
-
 
 
 
@@ -129,14 +55,14 @@ function all() {
 
 if (!qqreadbdArr[K-1]) {
 if (K>R)
-    $.msg(jsname+R+'到'+(K-1)+'打印成功'+K+'到'+Z+'打印失败', '⚠️提示：请先获取'+K+'到'+Z+'的ck','并在boxjs里设置好账号数')
+    $.msg(jsname+R+'到'+(K-1)+'打印成功'+K+'到'+Z+'打印失败', '⚠️提示：请先获取'+K+'到'+Z+'的Cookie','并在boxjs里设置好账号数')
 
 if (K==R&&Z>R)
-    $.msg(jsname+K+'到'+Z+'打印失败', '⚠️提示：请先获取'+K+'到'+Z+'的ck','并在boxjs里设置好账号数')
+    $.msg(jsname+K+'到'+Z+'打印失败', '⚠️提示：请先获取'+K+'到'+Z+'的Cookie','并在boxjs里设置好账号数')
 
 
 if (Z==R)
-    $.msg(jsname+K+'打印失败', '⚠️提示：请先获取'+K+'的ck','并在boxjs里设置好账号数')
+    $.msg(jsname+K+'打印失败', '⚠️提示：请先获取'+K+'的Cookie','并在boxjs里设置好账号数')
 
    $.done();
   }
@@ -168,7 +94,7 @@ if(timeheader==1)console.log(qqreadtimeheaderVal+GG)
               all();
 }    else if (K == Z ) {
           let F=Z-R+1
-              $.msg(jsname+R+'到'+Z+',ck打印成功','✅已打印'+F+'个ck',)
+              $.msg(jsname+R+'到'+Z+',Cookie打印成功','✅已打印'+F+'个Cookie',)
 
               $.done();
 
