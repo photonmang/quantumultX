50050a32b92bc1274d6d9dc94f9d10ef637af084
photonmang <42340256+photonmang@users.noreply.github.com>
2021-02-14 23:45:08 +0800

    Update xxys.js

--- xxys.js
+++ xxys.js
@@ -1,15 +1,16 @@
 /*
-更新：2020-10-23
+更新：2021-2-14
+*修复版本更新失效问题，请重新替换一次重写规则！
 *新增加小小影视老版本的广告去除方案。目前在使用老版本广告失效的，请重新更新下脚本并重新替换下rewrite_remote下的链接
 
 小小影视 unlock Vip&免广告
 感谢@GB-png，提供新方法
 QX:
-https:\/\/.*\..*\.com\/(ssp-svr\/ssp\/list3|ucp/index|getGlobalData) url script-response-body https://raw.githubusercontent.com/photonmang/quantumultX/master/xxys.js
+https:\/\/.*\.(xiaoxiaoapps|xiaoxiaoaimg)\.com\/(ssp-svr\/ssp\/list3|ucp/index|getGlobalData) url script-response-body https://raw.githubusercontent.com/photonmang/quantumultX/master/xxys.js
 Surge:
-http-response https:\/\/.*\..*\.com\/(ssp-svr\/ssp\/list3|ucp/index|getGlobalData) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/photonmang/quantumultX/master/xxys.js
+http-response https:\/\/.*\.(xiaoxiaoapps|xiaoxiaoaimg)\.com\/(ssp-svr\/ssp\/list3|ucp/index|getGlobalData) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/photonmang/quantumultX/master/xxys.js
 
-MITM = *.xxjjappss.com,*.huaerdadi.com
+MITM = *.xxjjappss.com,*.huaerdadi.com,*.xiaoxiaoapps.com,*.xiaoxiaoimg.com
 */
 
 const path1 = "/ucp/index";
