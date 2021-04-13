ea97dc13ffbe2e2f0f08187b0557f4613946519c
photonmang <42340256+photonmang@users.noreply.github.com>
2021-04-13 16:59:35 +0800

    Update jd_jdfactory.js

--- cache/jd/jd_jdfactory.js
+++ cache/jd/jd_jdfactory.js
@@ -53,7 +53,7 @@ if ($.isNode()) {
 }
 let wantProduct = ``;//心仪商品名称
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-const inviteCodes = [`P04z54XCjVWnYaS5u2ak7ZCdan1Bdd2GGiWvC6_uERj`, 'P04z54XCjVWnYaS5m9cZ2ariXVJwHf0bgkG7Uo'];
+const inviteCodes = [`T018v_56RRYR9FXXKR2b1ACjVWnYaS5kRrbA@T01476AsE0ZMq1zWJwCjVWnYaS5kRrbA@T014_qUsBEFHqAWJdwCjVWnYaS5kRrbA@T0225KkcRxZL8lTVKR7wkqQKIgCjVWnYaS5kRrbA@ml_MKIgCjVWnYaS5kRrbA`, 'T01556wuA0dEqgqDcUMCjVWnYaS5kRrbA@T0205KkcBUNfpwmqSWGP86hYCjVWnYaS5kRrbA'];
 !(async () => {
   await requireConfig();
   if (!cookiesArr[0]) {
