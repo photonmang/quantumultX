7990957a9358449b97c3be832467bcccf1a62838
photonmang <42340256+photonmang@users.noreply.github.com>
2021-05-04 15:38:03 +0800

    Update walq.js

--- JS/walq.js
+++ JS/walq.js
@@ -2,17 +2,24 @@
 我奥篮球 解锁付费直播回放及回放下载
 制作时间：2021/05/02
 制作者：photonmang
-1.本仅解锁付费音频部分，其他部分未进行解锁处理。后续看个人使用情况解锁更多VIP功能
-http://api.woaoo.net/schedule/getScheduleBasicInfo?\d+ url script-response-body https://raw.githubusercontent.com/photonmang/quantumultX/master/JS/walq.js
-MITM = api.woaoo.net
-
-
+http://api.woaoo.net/.+ url script-response-body https://raw.githubusercontent.com/photonmang/quantumultX/master/JS/walq.js
 */
-let walq = JSON.parse($request.body);
-walq.data.alreadyPaid = 'true';
-walq.data.alreadyPaidReplay = 'true';
-walq.data.alreadyPaidReplayDownload = 'true';
-walq.data.firstHighlightVideo.isPaid = 'true';
-walq.data.firstHighlightVideo.canWatchFree = 'true';
-walq.data.firstScheduleHighlight.isPaid = 'true';
-$done({body:JSON.stringify(walq)});
+
+re('"alreadyPaid":\\w+@"alreadyPaidReplay":\\w+@"alreadyPaidReplayDownload":\\d+@"isPaid":\\w+@"canWatchFree":\\w+@','"alreadyPaid":true@"alreadyPaidReplay":true@"alreadyPaidReplayDownload":true@"isPaid":true@"canWatchFree":true@')
+
+function re() {
+ var body = $response.body;
+ if (arguments[0].includes("@")) {
+  var regs = arguments[0].split("@");
+  var strs = arguments[1].split("@");
+  for (i = 0;i < regs.length;i++) {
+   var reg = new RegExp(regs[i],"g");
+   body = body.replace(reg, strs[i]);
+ }
+}
+ else {
+  var reg = new RegExp(arguments[0],"g");
+  body = body.replace(reg, arguments[1]);
+}
+ $done(body);
+} 
