31e94b1b66c2c43562621bfb762f3e13dd55511c
photonmang <42340256+photonmang@users.noreply.github.com>
2021-09-04 00:23:52 +0800

    Update zuoyebang.js

--- JS/zuoyebang.js
+++ JS/zuoyebang.js
@@ -10,7 +10,7 @@ MITM = apivip.zuoyebang.com
 */
 
 
-re('"freeTrail":\\w+@"status":\\d@"stopTime":""@','"freeTrail":1@"status":1@"stopTime":"9999999999"@')
+re('"freeTrail":\\d@"status":\\d@"stopTime":""@','"freeTrail":1@"status":1@"stopTime":"9999999999"@')
 
 function re() {
  var body = $response.body;
