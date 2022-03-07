a6dc106b4c1a5f213606d0d4e1df712750f8364e
photonmang <42340256+photonmang@users.noreply.github.com>
2022-03-07 11:29:49 +0800

    Update jy.js

--- JS/jy.js
+++ JS/jy.js
@@ -3,7 +3,7 @@
 鲸鱼视频 解锁付费观看
 制作时间：2022/03/07
 制作者：photonmang
-下载地址：https://6478726.com/96iru.html
+下载地址：https://jysp.app（官方是要下载描述文件），网页版：https://h5.7595vip06.com （不需要描述文件）
 QX:
 hostname = api.8897815.com
 https://api.8897815.com/long_video/user/video/(watch|buy) url script-request-body https://raw.githubusercontent.com/photonmang/quantumultX/master/JS/jy.js
